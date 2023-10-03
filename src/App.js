04b08547baa5bd3563fc572dadde1dfbafa32ea7
hassanelq <hassanelqadi2@gmail.com>
2023-10-03 20:42:46 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import shapeBlur from "./shapeBlur.webp";
+import moonLogo from "./moonLogo.svg";
 
 function App() {
   return (
@@ -6,6 +8,11 @@ function App() {
       <header className="App-header">
         <p>Coming soon ...</p>
       </header>
+      <img src={shapeBlur} className="blurBottom"></img>
+      <img src={shapeBlur} className="blurTop"></img>
+      <div className="Hero_right">
+        <img src={moonLogo} alt="moonlogo" className="moonLogo"></img>
+      </div>
     </div>
   );
 }
